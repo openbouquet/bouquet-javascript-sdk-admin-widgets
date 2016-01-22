0270de017922b32ae56556da329c07135a0eefb4
olivier <obalbous@squidsolutions.com>
2016-01-22 18:35:29 +0100

    T685 keep templates in templates.js and do not update /dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,17 +7,6 @@ module.exports = function(grunt) {
                 clean : {
                     all : "dist/"
                 },
-                bower_concat : {
-                    all : {
-                        dest : 'build/_bower.js',
-                        exclude : [ 'squid_api', "jquery",
-                                    "underscore",
-                                    "backbone"],
-                        bowerOptions : {
-                            relative : false
-                        }
-                    }
-                },
                 concat : {
                     options : {
                         stripBanners : true,
@@ -30,27 +19,6 @@ module.exports = function(grunt) {
                     css : {
                         src : [ 'src/*.css' ],
                         dest : 'dist/squid_api_admin-widgets.css',
-                    },
-                    dev: {
-                        files:{
-                            'dist/squid_api_collection_management_widget.js': ['dist/squid_api_collection_management_widget_template.js', 'src/squid_api_collection_management_widget.js'],
-                            'dist/squid_api_columns_management_widget.js': ['dist/squid_api_columns_management_widget_template.js', 'src/squid_api_columns_management_widget.js'],
-                            'dist/squid_api_model_management_widget.js': ['dist/squid_api_model_management_widget_template.js', 'src/squid_api_model_management_widget.js'],
-                            'dist/squid_api_project_management_widget.js': ['dist/squid_api_project_management_widget_template.js', 'src/squid_api_project_management_widget.js'],
-                            'dist/squid_api_relation_management_widget.js': ['dist/squid_api_relation_management_widget_template.js', 'src/squid_api_relation_management_widget.js'],
-                            'dist/squid_api_shortcuts_admin_widget.js': ['dist/squid_api_shortcuts_admin_widget_template.js', 'src/squid_api_shortcuts_admin_widget.js'],
-                            'dist/squid_api_users_admin_widget.js': ['dist/squid_api_users_admin_widget_template.js', 'src/squid_api_users_admin_widget.js']
-                        }
-                    }
-                },
-                copy: {
-                    devDist: {
-                        files: [{
-                            expand: true,
-                            flatten: true,
-                            src : [ 'src/*.js' ],
-                            dest: 'dist'
-                        }]
                     }
                 },
                 handlebars : {
@@ -65,17 +33,6 @@ module.exports = function(grunt) {
                         files : {
                             "build/templates.js" : [ "src/*.hbs" ]
                         }
-                    },
-                    dev : {
-                        files : {
-                            'dist/squid_api_collection_management_widget_template.js': ['src/squid_api_collection_management_widget.hbs'],
-                            'dist/squid_api_columns_management_widget_template.js': ['src/squid_api_columns_management_widget.hbs'],
-                            'dist/squid_api_model_management_widget_template.js': ['src/squid_api_model_management_widget.hbs'],
-                            'dist/squid_api_project_management_widget_template.js': ['src/squid_api_project_management_widget.hbs'],
-                            'dist/squid_api_relation_management_widget_template.js': ['src/squid_api_relation_management_widget.hbs'],
-                            'dist/squid_api_shortcuts_admin_widget_template.js': ['src/squid_api_shortcuts_admin_widget.hbs'],
-                            'dist/squid_api_users_admin_widget_template.js': ['src/squid_api_users_admin_widget.hbs']
-                        }
                     }
                 },
                 watch : {
@@ -89,11 +46,10 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-handlebars');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-bower-concat');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-copy');
 
-    grunt.registerTask('devDist', [ 'jshint', 'clean', 'handlebars:dev', 'copy:devDist', 'concat:dev']);
+    grunt.registerTask('dev', [ 'jshint', 'handlebars']);
 
     grunt.registerTask('default', [ 'jshint', 'clean', 'handlebars', 'concat:js', 'concat:css']);
 };
