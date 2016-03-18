c80a3ed9fb692ce79f48897e94802123e3346d16
Thomas Scott <thomas.scott@squidsolutions.com>
2016-03-18 15:18:24 +0100

    added sample pie chart viz

--- src/squid_api_dataviz_creator.js
+++ src/squid_api_dataviz_creator.js
@@ -221,6 +221,9 @@
                     }, {
                         id : "tableViz",
                         name : "Table"
+                    }, {
+                        id : "pieChartViz",
+                        name : "Pie Chart"
                     } ],
                     "headerText" : this.headerText
             };
@@ -252,6 +255,61 @@
             this.editor.getSession().setValue(this.editorContents(body));
 
             return this;
+        },
+
+        /**
+         * pie-chart datatviz sample code.
+         * Note : indentation was reduced on purpose
+         */
+        pieChartViz: function(analysis, el) {
+var elementWidth = d3.select("#" + el).node().getBoundingClientRect().width;
+var data = analysis.get("results").rows.slice(0, 5);
+
+var width = elementWidth,
+    height = 500,
+    radius = Math.min(width, height) / 2;
+
+var color = d3.scale.ordinal()
+    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+var arc = d3.svg.arc()
+    .outerRadius(radius - 10)
+    .innerRadius(0);
+
+var labelArc = d3.svg.arc()
+    .outerRadius(radius - 40)
+    .innerRadius(radius - 40);
+
+var pie = d3.layout.pie()
+    .sort(null)
+    .value(function(d) {
+        return d.v[1];
+    });
+
+var svg = d3.select("#" + el).append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+
+var g = svg.selectAll(".arc")
+    .data(pie(data))
+    .enter().append("g")
+    .attr("class", "arc");
+
+g.append("path")
+    .attr("d", arc)
+    .style("fill", function(d) {
+        return color(d.value);
+    });
+
+g.append("text")
+    .attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; })
+    .attr("dy", ".35em")
+    .text(function(d) {
+        return d.data.v[0];
+    });
         },
         
         /**
