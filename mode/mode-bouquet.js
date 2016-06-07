7478078f5cb0f505f24ce2d010c59f203f0fc2e8
Louis Rabiet <louis.rabiet@squidsolutions.com>
2016-06-07 17:12:33 +0200

    [T1102] Removing quote insertion

--- mode/mode-bouquet.js
+++ mode/mode-bouquet.js
@@ -763,69 +763,6 @@ ace.define("ace/mode/behaviour/cstyle",["require","exports","module","ace/lib/oo
                 }
             }
         });
-
-        this.add("string_dquotes", "insertion", function(state, action, editor, session, text) {
-            if (text == '"' || text == "'") {
-                initContext(editor);
-                var quote = text;
-                var selection = editor.getSelectionRange();
-                var selected = session.doc.getTextRange(selection);
-                if (selected !== "" && selected !== "'" && selected != '"' && editor.getWrapBehavioursEnabled()) {
-                    return getWrapped(selection, selected, quote, quote);
-                } else if (!selected) {
-                    var cursor = editor.getCursorPosition();
-                    var line = session.doc.getLine(cursor.row);
-                    var leftChar = line.substring(cursor.column-1, cursor.column);
-                    var rightChar = line.substring(cursor.column, cursor.column + 1);
-
-                    var token = session.getTokenAt(cursor.row, cursor.column);
-                    var rightToken = session.getTokenAt(cursor.row, cursor.column + 1);
-                    if (leftChar == "\\" && token && /escape/.test(token.type))
-                        return null;
-
-                    var stringBefore = token && /string|escape/.test(token.type);
-                    var stringAfter = !rightToken || /string|escape/.test(rightToken.type);
-
-                    var pair;
-                    if (rightChar == quote) {
-                        pair = stringBefore !== stringAfter;
-                    } else {
-                        if (stringBefore && !stringAfter)
-                            return null; // wrap string with different quote
-                        if (stringBefore && stringAfter)
-                            return null; // do not pair quotes inside strings
-                        var wordRe = session.$mode.tokenRe;
-                        wordRe.lastIndex = 0;
-                        var isWordBefore = wordRe.test(leftChar);
-                        wordRe.lastIndex = 0;
-                        var isWordAfter = wordRe.test(leftChar);
-                        if (isWordBefore || isWordAfter)
-                            return null; // before or after alphanumeric
-                        if (rightChar && !/[\s;,.})\]\\]/.test(rightChar))
-                            return null; // there is rightChar and it isn't closing
-                        pair = true;
-                    }
-                    return {
-                        text: pair ? quote + quote : "",
-                        selection: [1,1]
-                    };
-                }
-            }
-        });
-
-        this.add("string_dquotes", "deletion", function(state, action, editor, session, range) {
-            var selected = session.doc.getTextRange(range);
-            if (!range.isMultiLine() && (selected == '"' || selected == "'")) {
-                initContext(editor);
-                var line = session.doc.getLine(range.start.row);
-                var rightChar = line.substring(range.start.column + 1, range.start.column + 2);
-                if (rightChar == selected) {
-                    range.end.column++;
-                    return range;
-                }
-            }
-        });
-
     };
 
 
